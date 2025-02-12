e458a1a8d0a4a32f32a02b2b8c1aab5172d4720d
srikanth12345641 <motapothusrikanth0@gmail.com>
2025-02-12 13:14:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function display(value)
+{
+    document.getElementById('result').value+=value
+}
+function clearScreen()
+{
+    document.getElementById('result').value=""
+}
+function calculate(value)
+{
+   let p= document.getElementById('result').value
+   let q=eval(p)
+   document.getElementById('result').value=q
+}
+
